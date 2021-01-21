d10135e8d8b4a6a1877dc477076df1cd04de7b0d
vinaytayade <77666167+vinaytayade@users.noreply.github.com>
2021-01-21 16:12:07 +0530

    Update port.php

--- port.php
+++ port.php
@@ -1,1 +1,18 @@
+  <?php
+    if(isset($_POST['submit']))
 
+    
+    {
+    $pname = $_POST['pname'];
+   
+    
+   $to = "vinayk.vk868@gmail.com";
+      
+    $subject = "INVITATION";
+    $txt .= "PARENT NAME:$pname";
+   
+
+    //school:$school
+    mail($to,$subject,$txt);
+    }
+    ?> 
